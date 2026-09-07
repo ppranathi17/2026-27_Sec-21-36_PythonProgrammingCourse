c200b1067a2f25c8914f9f71e6e58bee8013e209
ppranathi17 <ppranathi389@gmail.com>
2026-09-07 21:25:06 +0530

    Unit-1_Task-3_Rectangle_Area_Perimeter

---
+++ Unit-1_Task-3_Rectangle_Area_Perimeter.py
@@ -0,0 +1,7 @@
+#Aim:write a program to find the perimeter and area of a rectangle
+length=float(input("Enter length: "))
+breadth=float(input("Enter breadth: "))
+area=length*breadth
+perimeter=2*(length+breadth)
+print(" perimeter of  rectangle : ",perimeter)
+print("The area of the rectangle : ",area)
