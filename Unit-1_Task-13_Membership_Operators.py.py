eff69362f182d471ddf995044489b49c20afd62a
ppranathi17 <ppranathi389@gmail.com>
2026-09-07 21:30:07 +0530

    Unit-1_Task-13_Membership_Operators

---
+++ Unit-1_Task-13_Membership_Operators.py.py
@@ -0,0 +1,13 @@
+#in operator
+b ='a' in 'apple'
+print("result of b is:" , b)
+
+a = 'o' in 'apple'
+print("result of a is:",a)
+
+#not in operator
+x = 'o'  not in 'orange'
+print("result of x:",x)
+
+c = 't' not in 'aeroplane'
+print("result of c:",c )
